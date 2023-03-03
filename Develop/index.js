bcf16f1b26d244a66a6274b30c6cc40899127619
Brett Hicklin <hicklin.brett@gmail.com>
2023-03-03 13:52:03 -0500

    added questions to be asked

--- Develop/index.js
+++ Develop/index.js
@@ -2,10 +2,22 @@
 const fs = require('fs');
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?",
+"Provide a short description of your project",
+"What are the steps required to install your project?",
+"Provide usage instructions",
+"Provide contribution guidelines, if any",
+"Provide test instructions, if any",
+"What licensing are you using?",
+"What is your GitHub username?",
+"What is your email address?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+  
+}
 
 // TODO: Create a function to initialize app
 function init() {}
