5534396a3dd2ab23c302fb7f6affc222afde918a
Brett Hicklin <hicklin.brett@gmail.com>
2023-03-05 12:25:17 -0500

    formatted index.js

--- Develop/index.js
+++ Develop/index.js
@@ -4,75 +4,73 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: "input",
-      message:"What is the title of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message:"Provide a short description of your project",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What are the steps required to install your project?",
-      name: "install",
-      default: "N/A",
-    },
-    {
-      type: "input",
-      message:"Provide usage instructions",
-      default: "N/A",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "Provide contribution guidelines, if any",
-      default: "N/A",
-      name: "contribution",
-    },
-    {
-      type: "input",
-      message:"Provide test instructions, if any",
-      default: "N/A",
-      name: "test",
-    },
-    {
-      type: "list",
-      message:"What licensing are you using?",
-      choices: ["MIT", "GPLv2", "Apache", "Other or None"],
-      name: "license",
-    },
-    {
-      type: "input",
-      message:"What is your GitHub username?",
-      name: "username",
-    },
-    {
-      type: "input",
-      message:"What is your email address?",
-      name: "email",
-    },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Provide a short description of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the steps required to install your project?",
+    name: "install",
+    default: "N/A",
+  },
+  {
+    type: "input",
+    message: "Provide usage instructions",
+    default: "N/A",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide contribution guidelines, if any",
+    default: "N/A",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Provide test instructions, if any",
+    default: "N/A",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "What licensing are you using?",
+    choices: ["MIT", "GPLv2", "Apache", "Other or None"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
 ];
 
-function userPrompts(){
-    inquirer
-    .prompt(questions)
-    .then((data) => {
-        writeToFile("README.md",data)
-    }
-)};
+function userPrompts() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", data);
+  });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-   
-      const readmeDoc = generateMarkdown.generateMarkdown(data);
+  const readmeDoc = generateMarkdown.generateMarkdown(data);
 
-      fs.writeFile(fileName, `${readmeDoc}`, (err) => {
-        err ? console.error(err) : console.log("You've successfully created a README file!");
-      });
-    
+  fs.writeFile(fileName, `${readmeDoc}`, (err) => {
+    err
+      ? console.error(err)
+      : console.log("You've successfully created a README file!");
+  });
 }
 
 // TODO: Create a function to initialize app
